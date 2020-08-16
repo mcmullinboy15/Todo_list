c470ed20fb502761324275d55f4b0e756e095219
Andrew McMullin <mcmullinboy15@gmail.com>
2020-08-16 09:28:17 -0600

    cleaned up and setup to work on Heroku

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todo_list
+
+This is a Django project using django's templating engine to service the fronted and backend
---
+++ testing/main.py
@@ -0,0 +1,7 @@
+from UserInterface import UserInterface
+
+host = "http://127.0.0.1:8000/todo/"
+host = "http://127.0.0.1:8003/todo/"
+
+ui = UserInterface( host )
+ui.run()
